408a221a6409358ae413b5b4b466e58af08d3aa5
Racso66 <dylan62015@hotmail.ca>
2020-11-05 09:33:07 -0800

    second

--- lita_config.rb
+++ lita_config.rb
@@ -17,7 +17,15 @@ Lita.configure do |config|
 
   # The adapter you want to connect with. Make sure you've added the
   # appropriate gem to the Gemfile.
-  config.robot.adapter = :shell
+  #heroku uses a RACK_ENV of 'production' by default
+  if ENV['RACK_ENV']=='production'
+    config.robot.adapter = :slack
+    config.redis[:url]=ENV.fetch('REDIS_URL')
+  else
+    config.robot.adapter = :shell
+  end
+  #Slack adapter demands a value even in dev when we aren't using it
+  config.adapter.slack.token = ENV.fetch('SLACK_TOKEN',"")
 
   ## Example: Set options for the chosen adapter.
   # config.adapter.username = "myname"
@@ -30,4 +38,5 @@ Lita.configure do |config|
   ## Example: Set configuration for any loaded handlers. See the handler's
   ## documentation for options.
   # config.handlers.some_handler.some_config_key = "value"
+
 end
